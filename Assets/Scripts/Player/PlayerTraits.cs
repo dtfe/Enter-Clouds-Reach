085b592a867da03e5338d006a6c28cfa6ec5a248
AskMake <haakon.magnusson@live.no>
2023-01-27 15:15:37 +0100

    Continuation on char sheet and creator

---
+++ Assets/Scripts/Player/PlayerTraits.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTraits : ScriptableObject
+{
+    public IDictionary<string,int[]> TraitList = new Dictionary<string,int[]>();
+
+}
