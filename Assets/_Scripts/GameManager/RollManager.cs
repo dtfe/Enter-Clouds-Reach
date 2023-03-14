ed4e4b6b4ae641e236700f298e58a3b2616dda0f
dtfe <355976@student.nord.no>
2023-03-14 16:47:45 +0100

    Post-processing, main menu scene

--- Assets/_Scripts/GameManager/RollManager.cs
+++ Assets/_Scripts/GameManager/RollManager.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class RollManager : MonoBehaviour
@@ -5,6 +7,9 @@ public class RollManager : MonoBehaviour
     private GameObject requestingEvent;
     private DieRoller DR;
     public PlayerStats ps;
+    private int rolledNumber;
+    private string rollType;
+    private int mod;
 
     private void Start()
     {
@@ -15,7 +20,8 @@ public class RollManager : MonoBehaviour
     public void rollAttack(GameObject requestingSystem)
     {
         requestingEvent = requestingSystem;
-        if (DR = null)
+        rollType = "attack";
+        if (DR == null)
         {
             DR = FindObjectOfType<DieRoller>();
         }
@@ -25,7 +31,8 @@ public class RollManager : MonoBehaviour
     public void rollAbilityCheck(GameObject rqEvent, string ability)
     {
         requestingEvent = rqEvent;
-        if (DR = null)
+        rollType = "ability";
+        if (DR == null)
         {
             DR = FindObjectOfType<DieRoller>();
         }
@@ -35,7 +42,8 @@ public class RollManager : MonoBehaviour
     public void rollDamage(GameObject requestingSystem, DieRoller.dFaces numbOfFaces)
     {
         requestingEvent = requestingSystem;
-        if (DR = null)
+        rollType = "damage";
+        if (DR == null)
         {
             DR = FindObjectOfType<DieRoller>();
         }
@@ -45,5 +53,21 @@ public class RollManager : MonoBehaviour
     public void giveResult(int resultNumber)
     {
         requestingEvent.GetComponent<IReceiveResult>().ReceiveRoll(resultNumber);
+        /*
+        switch (rollType)
+        {
+            case "ability":
+                requestingEvent.GetComponent<AbilityCheckScript>().ReceiveResult(resultNumber);
+                break;
+
+            case "attack":
+                requestingEvent.GetComponent<BattleSystem>().ReceiveRoll(resultNumber);
+                break;
+
+            case "damage":
+                requestingEvent.GetComponent<BattleSystem>().ReceiveRoll(resultNumber);
+                break;
+        }
+        */
     }
 }
