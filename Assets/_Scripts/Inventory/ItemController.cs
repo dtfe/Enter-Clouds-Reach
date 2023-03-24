5e693cc2cc777164f7aa6db7086d95b70d375c12
dtfe <355976@student.nord.no>
2023-03-24 15:09:37 +0100

    Inventory, Itemcontroller and item editor changes

---
+++ Assets/_Scripts/Inventory/ItemController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EnterCloudsReach.Inventory
+{
+    public class ItemController : MonoBehaviour
+    {
+        public Item item;
+    }
+}
